51202d70e63c307c23a8e536d02def9ae6f01e2c
David Ginsburg <david@daviddoodles.com>
2021-04-13 19:33:58 -0700

    add more options and implement playing different pronounciations by index

--- ru-pronounce.py
+++ ru-pronounce.py
@@ -8,6 +8,7 @@ import requests
 FILE_CACHE = os.path.expanduser('~/.ru-pronounce/cache/')
 PLAY_SOUND_CMD = 'afplay "{file}"'
 SOUND_FILE_EXT = '.mp3'
+FILEPATH_TEMPLATE = os.path.join(FILE_CACHE, '{word}' + '{suffix}' + SOUND_FILE_EXT)
 FORVO_URL = 'https://forvo.com/word/{ru_word}/#ru'
 AUDIO_URL = 'https://audio00.forvo.com/audios/mp3/{path}'
 FIND_ENCODED_AUDIO_ARGS_RE = 'Play\((\d+,[^)]*)'
@@ -24,25 +25,31 @@ def _setup_file_cache():
     if not os.path.isdir(FILE_CACHE):
         os.makedirs(FILE_CACHE, exist_ok=True)
 
-def pronounce(ru_word):
-    soundfile = ru_word + SOUND_FILE_EXT
-    filepath = os.path.join(FILE_CACHE, soundfile)
+def download_if_not_available(ru_word):
+    filepath = _get_word_filepath(ru_word)
+    if not os.path.isfile(filepath):
+        logging.info(f'File {soundfile} not cached. Downloading...')
+        download(ru_word)
+
+def _get_word_filepath(ru_word, index=0):
+    suffix = '' if index == 0 else f'_{index}'
+    return FILEPATH_TEMPLATE.format(word=ru_word, suffix=suffix)
+
+def pronounce(ru_word, pronunciation_index=0):
+    filepath = _get_word_filepath(ru_word, pronunciation_index)
     if os.path.isfile(filepath):
         play(filepath)
     else:
-        logging.info(f'File {soundfile} not cached. Downloading...')
-        download(ru_word, FILE_CACHE)
-        play(filepath)
+        logging.error(f'File {filepath} does not exist')
 
 def play(filepath):
     if os.path.isfile(filepath):
         logging.debug(f'Playing file {filepath}')
         os.system(PLAY_SOUND_CMD.format(file=filepath))
     else:
-        logging.warning(f'Could not find {filepath}')
+        logging.error(f'Could not find {filepath}')
 
-def download(ru_word, dirpath):
-    filepath_template = os.path.join(FILE_CACHE, ru_word + '{suffix}' + SOUND_FILE_EXT)
+def download(ru_word):
     audio_urls = []
     headers = {
         'User-Agent': USER_AGENT
@@ -66,8 +73,7 @@ def download(ru_word, dirpath):
         logging.debug(f'Downloading {url}...')
         r = requests.get(url, headers=headers)
         if r.status_code == 200:
-            suffix = '' if i == 0 else f'_{i}'
-            filepath = filepath_template.format(suffix=suffix)
+            filepath = _get_word_filepath(ru_word, i)
             logging.debug(f'Download successful. Saving to {filepath}')
             with open(filepath, 'wb') as fd:
                 fd.write(r.content)
@@ -80,11 +86,42 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('ru_word')
     parser.add_argument(
-            '-d', '--debug', action='store_true', help='print debug messages')
+            '-d',
+            '--debug',
+            action='store_true',
+            help='print debug messages'
+            )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+            '-c',
+            '--cycle',
+            nargs='?',
+            type=int,
+            const=-1,
+            default=None,
+            help='cycle through available word pronounciations.' \
+            ' If an integer n is specified, cycle through the first' \
+            ' n available pronounciations'
+            )
+    group.add_argument(
+            '-n',
+            '--play-n',
+            type=int,
+            help='play the nth available word pronounciation' \
+            ' where n is the integer provided to this option'
+            )
+    group.add_argument(
+            '-r',
+            '--random',
+            action='store_true',
+            help='play a random available word pronounciation'
+            )
+
     args = parser.parse_args()
     log_level = logging.INFO
     if args.debug:
         log_level = logging.DEBUG
     _setup_logging(log_level)
     _setup_file_cache()
-    pronounce(args.ru_word)
+    download_if_not_available(args.ru_word)
+    pronounce(args.ru_word, pronunciation_index=args.play_n)
